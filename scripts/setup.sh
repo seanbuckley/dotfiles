461df42c2b9157b52c3cfd2cddf33a11b45cd934
Sean Buckley <seanbuckley@users.noreply.github.com>
2020-08-12 13:34:57 -0700

    Add warning about dup config of username and email

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -63,7 +63,7 @@ rm -rf ${HOME}/wsl-setup.tar.gz
 rm -rf ${HOME}/wsl-setup.zip
 #rm -rf ${DOTFILES_DIRECTORY}
 
-# Set Git username and email
+# Set Git username and email (already set in .gitconfig file)
 e_header "Setting Git username and email"
 git config --global user.name "Sean Buckley"
 git config --global user.email "seanbuckley@users.noreply.github.com"
