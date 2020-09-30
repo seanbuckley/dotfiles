c70375247c4a7e96442c9894e5fce1183e880a0e
Sean Buckley <seanbuckley@users.noreply.github.com>
2020-09-30 16:23:11 -0700

    Source nvm for bash script continuation

--- scripts/node-install.sh
+++ scripts/node-install.sh
@@ -15,8 +15,12 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 # This kills the install process
 #exec zsh -l
 
+# Source NVM to use in current process
+source $HOME/.nvm/nvm.sh;
+
 # Install the latest Node.js with NVM
 nvm install --lts
 nvm use --lts
+nvm install-latest-npm
 
 e_success "Completed node-install.sh"
